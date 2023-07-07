ed820a23df87403ece5eeb76e9cd8e5ce071a52e
Munjoo Kim <munjoo.edu@gmail.com>
2023-07-07 11:13:25 +0900

    [F] refactor read function
    Signed-off-by: Munjoo Kim <munjoo.edu@gmail.com>

--- Project1/DeviceDriver.cpp
+++ Project1/DeviceDriver.cpp
@@ -5,16 +5,27 @@ using namespace std;
 DeviceDriver::DeviceDriver(FlashMemoryDevice *hardware) : m_hardware(hardware)
 {}
 
+unsigned char DeviceDriver::read_from_device(long address)
+{
+	return m_hardware->read(address);
+}
+
+void DeviceDriver::check_data_4times(long address, unsigned char ret)
+{
+	for(int i= 0; i<4; i++)
+	{
+		if (ret != m_hardware->read(address))
+			throw exception("HW fault");
+	}
+}
+
 int DeviceDriver::read(long address)
 {
     unsigned char ret;
 
-    ret = m_hardware->read(address);
-    for(int i= 0; i<4; i++)
-    {
-        if (ret != m_hardware->read(address))
-            throw exception("HW fault");
-    }
+    ret = read_from_device(address);
+    check_data_4times(address, ret);
+
     return (int)ret;
 }
 
--- Project1/DeviceDriver.h
+++ Project1/DeviceDriver.h
@@ -5,6 +5,8 @@ class DeviceDriver
 {
 public:
     DeviceDriver(FlashMemoryDevice *hardware);
+    unsigned char read_from_device(long address);
+    void check_data_4times(long address, unsigned char ret);
     int read(long address);
     void write(long address, int data);
 
