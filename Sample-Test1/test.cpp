20abb9f2a7346e11ac713849d918d9418d8b2841
Munjoo Kim <munjoo.edu@gmail.com>
2023-07-07 10:06:02 +0900

    Add init code
    Signed-off-by: Munjoo Kim <munjoo.edu@gmail.com>

---
+++ Sample-Test1/test.cpp
@@ -0,0 +1,11 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "../project1/DeviceDriver.h"
+using namespace testing;
+using namespace std;
+
+
+TEST(TestCaseName, TestName) {
+	EXPECT_THAT(0, Eq(0));
+
+}
