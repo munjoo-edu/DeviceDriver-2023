1a7da735da39d452be9d9c8fc1b1b3d5f67af184
Munjoo Kim <munjoo.edu@gmail.com>
2023-07-07 11:01:05 +0900

    [G] Add normal read test
    Signed-off-by: Munjoo Kim <munjoo.edu@gmail.com>

--- Project1/DeviceDriver.cpp
+++ Project1/DeviceDriver.cpp
@@ -1,12 +1,21 @@
 #include "DeviceDriver.h"
+#include <stdexcept>
+using namespace std;
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice *hardware) : m_hardware(hardware)
 {}
 
 int DeviceDriver::read(long address)
 {
-    // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    unsigned char ret;
+
+    ret = m_hardware->read(address);
+    for(int i= 0; i<4; i++)
+    {
+        if (ret != m_hardware->read(address))
+            throw exception("HW fault");
+    }
+    return (int)ret;
 }
 
 void DeviceDriver::write(long address, int data)
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,11 +1,28 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
-#include "../project1/DeviceDriver.h"
+#include "../project1/DeviceDriver.cpp"
 using namespace testing;
 using namespace std;
 
+// make r/w mock function
+class MockFlashDD : public FlashMemoryDevice
+{
+public:
+	//virtual unsigned char read(long address) = 0;
+	//virtual void write(long address, unsigned char data) = 0;
+	MOCK_METHOD(unsigned char, read, (long address), (override));
+	MOCK_METHOD(void, write, (long address, unsigned char data), (override));
+};
 
-TEST(TestCaseName, TestName) {
-	EXPECT_THAT(0, Eq(0));
+TEST(TestCaseName, TestReadNormal) {
+	MockFlashDD mock{};
+	EXPECT_CALL(mock, read(0x01))
+		.Times(5)
+		.WillRepeatedly(Return('A'));
 
+	DeviceDriver dd{ &mock };
+
+	unsigned char actual = dd.read(0x01);
+	unsigned char expected = 'A';
+	EXPECT_EQ(expected, actual);
 }
